0858f6043ece6c367a78c298c5c7c9b0c317b053
DeanCaly <deancaly@gmail.com>
2026-07-23 20:39:53 -0300

    feat: add cep no found handling

--- main.py
+++ main.py
@@ -12,22 +12,26 @@ def validate_cep():
 
 
 def get_address(cep):
-       url = f"https://viacep.cm.br/ws/{cep}/json/"
+       url = f"https://viacep.com.br/ws/{cep}/json/"
        try:
               response = requests.get(url, timeout=5)
        except requests.exceptions.ConnectionError:
               print("Erro de conexão. Verifique sua internet e tente novamente.")
-              return None
        except requests.exceptions.Timeout:
               print("Indisponível no momento! Tente conectar novamente em alguns minutos")
               
        data = response.json()
+       
+       if "erro" in data:
+              print("cep não encontrado")
+              return None
        return data
        
        
 cep = validate_cep()
 
 data = get_address(cep)
+
 
 info = f"Cep: {data['cep']}\n"\
        f"Logradouro: {data['logradouro']}\n"\
